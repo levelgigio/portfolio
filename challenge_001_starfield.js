39fff30dcde28d5adeef7aa493f166018e4e6df6
NEOGIGIO <gigio.forastieri@gmail.com>
2018-08-23 01:52:58 -0300

    instace moded p5 sketchs

--- challenge_001_starfield.js
+++ challenge_001_starfield.js
@@ -1,51 +1,55 @@
-function Star() {
-    this.x = random(-largura/2, largura/2);
-    this.y = random(-altura/2, altura/2);
-    this.z = random(0, altura/2);
-    this.px = 0;
-    this.py = 0;
-    this.draw = function() {
-        fill(255);
-        noStroke();
-        var tamanho = map(this.z, 0, altura/2, 6, 0);
-        ellipse(this.x/this.z*largura, this.y/this.z*altura, tamanho, tamanho);
-        stroke(255);
-        strokeWeight(tamanho);
-        line(this.px, this.py, this.x/this.z*largura, this.y/this.z*altura);
-    };
-    this.update = function() {
-        this.px = this.x/this.z*largura;
-        this.py = this.y/this.z*altura;
-        this.z = this.z - velocidade;
-        
-        if(this.z < 1) {
-            this.z = random(0, altura/2);  
+var starfield = new p5((sketch) => {
+
+    function Star() {
+        this.x = sketch.random(-largura/2, largura/2);
+        this.y = sketch.random(-altura/2, altura/2);
+        this.z = sketch.random(0, altura/2);
+        this.px = 0;
+        this.py = 0;
+        this.draw = function() {
+            sketch.fill(255);
+            sketch.noStroke();
+            var tamanho = sketch.map(this.z, 0, altura/2, 6, 0);
+            sketch.ellipse(this.x/this.z*largura, this.y/this.z*altura, tamanho, tamanho);
+            sketch.stroke(255);
+            sketch.strokeWeight(tamanho);
+            sketch.line(this.px, this.py, this.x/this.z*largura, this.y/this.z*altura);
+        };
+        this.update = function() {
             this.px = this.x/this.z*largura;
             this.py = this.y/this.z*altura;
-        } 
-    };
-}
+            this.z = this.z - velocidade;
 
-// ------------------------------------------------------------- //
+            if(this.z < 1) {
+                this.z = sketch.random(0, altura/2);  
+                this.px = this.x/this.z*largura;
+                this.py = this.y/this.z*altura;
+            } 
+        };
+    }
 
-let altura = 400;
-let largura = 400;
-var stars = [];
-var velocidade = 5;
+    // ------------------------------------------------------------- //
 
-function setup() {
-    var canvas = createCanvas(largura, altura);
-    canvas.parent("challenge_001_starfield");
-    background(0);
-    for(var i = 0; i < 600; i++)
-        stars[i] = new Star();
-}
+    let altura = 400;
+    let largura = 400;
+    var stars = [];
+    var velocidade = 5;
 
-function draw() {
-    background(0);
-    translate(largura/2, altura/2);
-    for(var i = 0; i < 600; i++) {
-        stars[i].update();
-        stars[i].draw();
-    }       
-}
+
+    sketch.setup = function() {
+        var canvas = sketch.createCanvas(largura, altura);
+        canvas.parent("challenge_001_starfield");
+        sketch.background(0);
+        for(var i = 0; i < 600; i++)
+            stars[i] = new Star();
+    }
+
+    sketch.draw = function() {
+        sketch.background(0);
+        sketch.translate(largura/2, altura/2);
+        for(var i = 0; i < 600; i++) {
+            stars[i].update();
+            stars[i].draw();
+        }       
+    }
+});
--- challenge_004_purple_rain.js
+++ challenge_004_purple_rain.js
@@ -1,74 +1,76 @@
-function Gota() {
-    this.x = random(0, largura);
-    this.y = random(-altura, 0);
-    this.z = random(0, 20);
-    this.comprimento = map(this.z, 0, 20, 4, 10);
-    this.espessura = map(this.z, 0, 20, 1, 3)
-    this.velocidade = map(this.z, 0, 20, 2, 5);
-    this.draw = function() {
-        stroke(100, 0, 100);
-        strokeWeight(this.espessura);
-        line(this.x, this.y, this.x ,this.y+this.comprimento);
+var purple_rain = new p5((sketch) => {
+    function Gota() {
+        this.x = sketch.random(0, largura);
+        this.y = sketch.random(-altura, 0);
+        this.z = sketch.random(0, 20);
+        this.comprimento = sketch.map(this.z, 0, 20, 4, 10);
+        this.espessura = sketch.map(this.z, 0, 20, 1, 3)
+        this.velocidade = sketch.map(this.z, 0, 20, 2, 5);
+        this.draw = function() {
+            sketch.stroke(100, 0, 100);
+            sketch.strokeWeight(this.espessura);
+            sketch.line(this.x, this.y, this.x ,this.y+this.comprimento);
+        }
+        this.update = function() {
+            this.velocidade *= 1.01;
+            this.y += this.velocidade;
+            if(this.y >= altura) {
+                splashs.push(new Splash(this));
+                this.y = sketch.random(-altura, 0);
+                this.x = sketch.random(0, largura);
+                this.velocidade = sketch.map(this.z, 0, 20, 2, 5);
+            }    
+        }
     }
-    this.update = function() {
-        this.velocidade *= 1.01;
-        this.y += this.velocidade;
-        if(this.y >= altura) {
-            splashs.push(new Splash(this));
-            this.y = random(-altura, 0);
-            this.x = random(0, largura);
-            this.velocidade = map(this.z, 0, 20, 2, 5);
-        }    
+
+    function Splash(gota) {
+        this.x1 = gota.x;
+        this.x2 = gota.x
+        this.y = gota.y;
+        this.espessura = gota.espessura;
+        this.mod_velocidade = -gota.velocidade/1.5;
+        this.angulo = sketch.map(gota.velocidade, 2, 5, 63.0*3.14/180.0, 70.0*3.14/180.0);
+        this.velx = Math.cos(this.angulo)*this.mod_velocidade;
+        this.vely = Math.sin(this.angulo)*this.mod_velocidade;
+        this.draw = function() {
+            sketch.noFill();
+            sketch.strokeWeight(1);
+            sketch.ellipse(this.x1, this.y, this.espessura, this.espessura);
+            sketch.ellipse(this.x2, this.y, this.espessura, this.espessura);
+        }
+        this.update = function() {
+            this.vely += 1;
+            this.y += this.vely;
+            this.x1 += this.velx;
+            this.x2 -= this.velx;
+        }
     }
-}
 
-function Splash(gota) {
-    this.x1 = gota.x;
-    this.x2 = gota.x
-    this.y = gota.y;
-    this.espessura = gota.espessura;
-    this.mod_velocidade = -gota.velocidade/1.5;
-    this.angulo = map(gota.velocidade, 2, 5, 63.0*3.14/180.0, 70.0*3.14/180.0);
-    this.velx = Math.cos(this.angulo)*this.mod_velocidade;
-    this.vely = Math.sin(this.angulo)*this.mod_velocidade;
-    this.draw = function() {
-        noFill();
-        strokeWeight(1);
-        ellipse(this.x1, this.y, this.espessura, this.espessura);
-        ellipse(this.x2, this.y, this.espessura, this.espessura);
+    // ------------------------------------------------------------- //
+
+    let largura = 400;
+    let altura = 400;
+    var gotas = [];
+    var splashs = [];
+
+    sketch.setup = function() {
+        var canvas = sketch.createCanvas(largura, altura);
+        canvas.parent("challenge_004_purple_rain");
+        for (var i = 0; i < 35; i++) 
+            gotas.push(new Gota());
     }
-    this.update = function() {
-        this.vely += 1;
-        this.y += this.vely;
-        this.x1 += this.velx;
-        this.x2 -= this.velx;
+
+    sketch.draw = function() {
+        sketch.background(255, 218, 185);
+        for (var i = 0; i < gotas.length; i++) {
+            gotas[i].draw();
+            gotas[i].update();
+        } 
+        for (var i = 0; i < splashs.length; i++) {
+            splashs[i].draw();
+            splashs[i].update();
+            if(splashs[i].y > altura + 10)
+                splashs.splice(i, 1);
+        }
     }
-}
-
-// ------------------------------------------------------------- //
-
-let largura = 400;
-let altura = 400;
-var gotas = [];
-var splashs = [];
-
-function setup() {
-    var canvas = createCanvas(largura, altura);
-    canvas.parent("challenge_004_purple_rain");
-    for (var i = 0; i < 35; i++) 
-        gotas.push(new Gota());
-}
-
-function draw() {
-    background(255, 218, 185);
-    for (var i = 0; i < gotas.length; i++) {
-        gotas[i].draw();
-        gotas[i].update();
-    } 
-    for (var i = 0; i < splashs.length; i++) {
-        splashs[i].draw();
-        splashs[i].update();
-        if(splashs[i].y > altura + 10)
-            splashs.splice(i, 1);
-    }
-}
+});
